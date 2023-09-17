67d6c979e36d659e6915526ec86a2bb535e97ba0
Linhp3399 <133502958+Linhp3399@users.noreply.github.com>
2023-09-17 11:58:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+numer={
+    "mama": 1234456,
+    "tata": 1111111,
+    "babcia": 658988,
+}
+word = input("kogo numer chcesz otrzymać(małe litery)")
+if word in numer.keys():
+    print(word, numer[word])
+else:
+    print("nie ma przypisanego do tego numeru")
 
